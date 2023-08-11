f919c964b721401ffd5db69c2c1506ddc0c91811
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-11 14:46:05 -0700

    reorganized const declarations

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,13 +4,12 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
-
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
-const reservationsRouter = require("./reservations/reservations.router");
-const tablesRouter = require("./tables/tables.router")
-
 const app = express();
+
+const reservationsRouter = require("./reservations/reservations.router");
+const tablesRouter = require("./tables/tables.router");
 
 app.use(cors());
 app.use(express.json());
