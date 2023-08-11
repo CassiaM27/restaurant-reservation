45f3f5286cf19ab55211f6ca6464336d97ef6324
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-11 14:38:42 -0700

    initialized tables backend

---
+++ back-end/tables/tables.controller.js
@@ -0,0 +1,54 @@
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+function bodyHas(req, res, next) {
+  const { data } = req.body;
+
+  if(data === {}) {
+    return next({ status: 400, message: `Request must include data` });
+  } else {
+    return next();
+  }
+}
+
+async function tableExists(req, res, next) {
+  const table = await service.read(req.params.tableId);
+  if(table) {
+    res.locals.table = table;
+    return next();
+  } else {
+    return next({ status: 404, message: `id not found: ${req.params.tableId}`})
+  }
+}
+
+async function create(req, res) {
+  const newTable= req.body.data;
+  const data = await service.create(newTable)
+  res.status(201).json({ data: data });
+}
+
+/**
+ * List handler for table resources
+ */
+async function list(req, res) {
+  const data = await service.list();
+  res.status(201).json({ data });
+
+}
+
+async function read(req, res) {
+  res.status(201).json({ data: await service.read(req.params.tableId) });
+}
+
+async function update(req, res) {
+  const data = req.body.data;
+  const updatedTable = await service.update(data)
+  res.status(201).json({ data: updatedTable })
+}
+
+module.exports = {
+  create: [bodyHas, asyncErrorBoundary(create)],
+  list: asyncErrorBoundary(list),
+  read: [asyncErrorBoundary(tableExists), asyncErrorBoundary(read)],
+  update: [bodyHas, asyncErrorBoundary(update)],
+};
---
+++ back-end/tables/tables.service.js
@@ -0,0 +1,31 @@
+const knex = require("../db/connection");
+
+async function list() {
+  return knex("tables").select("*");
+}
+
+async function create(newTable) {
+  return knex("tables")
+    .insert(newTable, "*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+async function read(id) {
+  return knex("tables as t")
+    .select("*")
+    .where({ "t.table_id": id })
+    .first();
+}
+
+async function update(updatedTable) {
+  return knex("tables as t")
+    .insert(updatedTable, "*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+module.exports = {
+  list,
+  create,
+  read,
+  update,
+}
