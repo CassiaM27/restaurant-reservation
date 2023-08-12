cfd296d52c557dd39b386c246811a1c11b62792d
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-12 11:26:14 -0700

    revised list and listByDate orderBy methods

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,13 +1,14 @@
 const knex = require("../db/connection");
 
 async function list() {
-  return knex("reservations").select("*");
+  return knex("reservations").select("*").orderBy("reservation_time");
 }
 
 async function listByDate(date) {
   return knex("reservations as r")
     .select("*")
     .where({"r.reservation_date": date})
+    .orderBy("reservation_time")
 }
 async function create(newReservation) {
   return knex("reservations")
