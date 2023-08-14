f4969e11d199adbbc85eadde2208567ce71cada0
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-14 14:21:05 -0700

    moved initialFormState to ReservationForm

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -3,19 +3,10 @@ import ReservationForm from "./ReservationForm";
 
 function NewReservation() {
 
-    const initialFormState = {
-        first_name: "",
-        last_name: "",
-        mobile_number: "",
-        reservation_date: "",
-        reservation_time: "",
-        people: "",
-    }
-
     return (
         <div>
             <h1 className="my-3">New Reservation</h1>
-            <ReservationForm initialFormState={initialFormState} />
+            <ReservationForm />
         </div>
     )
 }
