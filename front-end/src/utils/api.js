2d565f5107c843e56a1cbc51337f4602614c2850
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-15 11:22:34 -0700

    revised readReservation

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -102,23 +102,12 @@ export async function changeReservation(reservation, signal) {
  * Lists a single reservation from the database
  * 
  */
-export async function showReservation(reservation, signal) {
-  const url = `${API_BASE_URL}/reservations`;
-  const options = {
-    method: "GET",
-    headers,
-    signal,
-  }
-  return await fetchJson(url, options, reservation)
-}
-/*
 export async function readReservation(reservation_id, signal) {
   const url = `${API_BASE_URL}/reservations/${reservation_id}`;
   return await fetchJson(url, { headers, signal }, [])
     .then(formatReservationDate)
     .then(formatReservationTime);
 }
-*/
 
 /**
  * Updates a reservation and saves it to the database
