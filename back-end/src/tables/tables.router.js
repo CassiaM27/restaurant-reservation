3f47fd1deb963310c1c1e8bb7f0370e2f432287a
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-15 14:58:09 -0700

    updated /:table_id/seat route

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -8,16 +8,9 @@ const router = require("express").Router();
 const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-const reservationsRouter = require("../reservations/reservations.router")
-const seatReservation = router.use("/reservations/:reservation_id/tables/:table_id/seat", reservationsRouter)
-
-router.route(seatReservation)
-  .put(controller.seat)
-  .all(methodNotAllowed);
-
-router.route("/:table_id")
-  .get(controller.read)
+router.route("/:table_id/seat")
   .put(controller.update)
+  .delete(controller.finish)
   .all(methodNotAllowed);
 
 router.route("/")
