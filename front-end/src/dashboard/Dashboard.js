9f005648b15ed656f530a6e4c8c03718f9739bad
Cassia McDonnell <cassiamcd27@gmail.com>
2023-08-17 09:49:01 -0700

    revised useEffect + styling, added ListTables

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { listReservations, listTables, finishTable, updateStatus } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import ShowAllErrors from "../layout/ShowAllErrors";
 import { previous, today, next } from "../utils/date-time";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import ListReservations from "../reservations/ListReservations";
-//import ListTables from "../tables/ListTables";
-
-
+import ListTables from "../tables/ListTables";
 
 /**
  * Defines the dashboard page.
@@ -15,40 +14,15 @@ import ListReservations from "../reservations/ListReservations";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-  const query = new URLSearchParams(useLocation().search);
-  const viewDate = query.get("date")
-  
+
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
-  const [reservationDate, setReservationDate] = useState(date);
+  const [tablesError, setTablesError] = useState(null);
   const [tables, setTables] = useState([]);
   const history = useHistory();
   const filterResults = true;
 
-  //useEffect(loadDashboard, [date]);
-
-  /*
-  if(viewDate !== null && viewDate !== date) {
-    setReservationDate(viewDate)
-  }
-  */
-
-  useEffect(() => {
-    const Abort = new AbortController();
-    async function loadDashboard() {
-    try {
-      setReservationsError(null);
-      setReservationDate(viewDate)
-      const list = await listReservations({reservationDate}, Abort.signal)
-      setReservations(list)
-    }
-    catch (error) {
-      setReservationsError(error);
-    }
-    return () => Abort.abort();
-  }
-  loadDashboard(reservationDate)
-  }, [viewDate, reservationDate]);
+  useEffect(loadDashboard, [date]);
 
   function loadDashboard() {
     const Abort = new AbortController();
@@ -58,90 +32,82 @@ function Dashboard({ date }) {
       .then(setReservations)
       .catch(setReservationsError);
 
-    listTables().then(setTables);
+    listTables(Abort.signal)
+      .then((tables) =>
+        tables.sort((tableA, tableB) => tableA.table_id - tableB.table_id)
+      )
+      .then(setTables)
+      .catch(setTablesError);
 
     return () => Abort.abort();
   }
 
-const handleCancel = async (event) => {
-  const result = window.confirm("Do you want to cancel this reservation? This cannot be undone.");
-  if (result) {
-    await updateStatus(event.target.value, "cancelled");
-    loadDashboard();
-  }
-};
-
-async function handleFinish(table_id) {
-  const Abort = new AbortController();
-  const result = window.confirm("Is this table ready to seat new guests? This cannot be undone.");
-
-  if (result) {
-    await finishTable(table_id, Abort.signal);
-    loadDashboard();
-  }
-  return () => Abort.abort();
-}
-
-/*
-  const allReservations = reservations.map((reservation) => {
-    if(reservation.reservation_date === date) {
-      return (
-        <div className="m-2 border rounded pt-2 row">
-          <div className="col-6">
-            <p className="mx-3">Name: {reservation.last_name}, {reservation.first_name}</p>
-            <p className="mx-3">Phone Number: {reservation.mobile_number}</p>
-          </div>
-          <div className="col-6">
-            <p className="">Day: {formatAsDate(reservation.reservation_date)}</p>
-            <p className="">Time: {formatAsTime(reservation.reservation_time)}</p>
-            <p className="">People: {reservation.people}</p>
-          </div>
-        </div>
-      )
+  const handleCancel = async (event) => {
+    const result = window.confirm("Do you want to cancel this reservation? This cannot be undone.");
+    if (result) {
+      await updateStatus(event.target.value, "cancelled");
+      loadDashboard();
     }
-  })
-*/
+  };
+
+  async function handleFinish(table_id) {
+    const Abort = new AbortController();
+    const result = window.confirm("Is this table ready to seat new guests? This cannot be undone.");
+
+    if (result) {
+      await finishTable(table_id, Abort.signal);
+      loadDashboard();
+    }
+    return () => Abort.abort();
+  }
 
   return (
     <main>
-      <ErrorAlert error={reservationsError} />
+      <ShowAllErrors errors={reservationsError} />
       <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h3 className="mb-0">Reservations for {reservationDate}</h3>
-      </div>
-      <div>
-        <button
-          className="btn btn-light border ml-2"
-          onClick={() => history.push(`/dashboard?date=${previous(viewDate)}`)}
-        >
-          Previous
-        </button>
-        <button
-          className="btn btn-light border mx-2"
-          onClick={() => history.push(`/dashboard?date=${today()}`)}
-        >
-          Today
-        </button>
-        <button
-          className="btn btn-light border"
-          onClick={() => history.push(`/dashboard?date=${next(viewDate)}`)}
-        >
-          Next
-        </button>
+      <div className="row">
+        <div className="d-md-flex mb-3 col">
+          <h3 className="mb-0">Reservations for {date}</h3>
+        </div>
+        <div className="mr-4">
+          <button
+            className="btn btn-light border ml-2"
+            onClick={() => history.push(`/dashboard?date=${previous(date)}`)}
+          >
+            Previous
+          </button>
+          <button
+            className="btn btn-light border mx-2"
+            onClick={() => history.push(`/dashboard?date=${today()}`)}
+          >
+            Today
+          </button>
+          <button
+            className="btn btn-light border"
+            onClick={() => history.push(`/dashboard?date=${next(date)}`)}
+          >
+            Next
+          </button>
+        </div>
       </div>
       <hr></hr>
       <div id="reservations" className="col-12">
         <ListReservations
           reservations={reservations}
+          date={date}
           filterResults={filterResults}
           handleCancel={handleCancel}
         />
       </div>
-
+      <br/>
       <div id="tables" className="item">
         <h3>Tables</h3>
+        <ErrorAlert error={tablesError} />
         <hr></hr>
-        {/*<ListTables tables={tables} handleFinish={handleFinish} />*/}
+        <ListTables
+          tables={tables}
+          handleFinish={handleFinish}
+          />
       </div>
     </main>
   );
